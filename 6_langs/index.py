eb04c2c345ba3fca8824a6c996edd0658cdae8ee
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-11-15 14:26:34 +0100

    Update desc for task 6

--- 6_langs/index.py
+++ 6_langs/index.py
@@ -1,5 +1,7 @@
 """
 1. Tworzę 1-, 2-, 3-gramy (może być więcej) dla obu porównywanych języków (tekstów). W ten sposób powstaje `x` i `y`.
+   Porównuję tylko ngramy o takiej samej krotności! Czyli 1-gramy `x` z 1-gramami `y` itd.
+   Są one częścią tego samego wektora, np. { 'a': 312, ..., 'ab': 12, ... }
 2. x1 i y1 to odpowiadające sobie ngramy, np. 'al' i 'al' – a dokładniej ich wartości (częstością występowania).
 3. Jeżeli dany ngram nie wystąpi w drugim tekście, to jego wartość powinna być przyjęta jako 0.
 
