e21643237f434dc4dbf1648aaf8d957016269bb9
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-11-15 14:19:34 +0100

    Prepare for 6th task

---
+++ 6_langs/index.py
@@ -0,0 +1,7 @@
+"""
+1. Tworzę 1-, 2-, 3-gramy (może być więcej) dla obu porównywanych języków (tekstów). W ten sposób powstaje `x` i `y`.
+2. x1 i y1 to odpowiadające sobie ngramy, np. 'al' i 'al' – a dokładniej ich wartości (częstością występowania).
+3. Jeżeli dany ngram nie wystąpi w drugim tekście, to jego wartość powinna być przyjęta jako 0.
+
+Długość wektora len(x): √x1^2 + x2^2 + x3^2 + ... + xn^2 (pierwiastek nad całością)
+"""
