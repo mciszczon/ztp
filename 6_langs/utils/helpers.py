3dfacb2ec3b2f82f00e0fdd1a4b302ca1e8659ba
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-11-19 17:30:01 +0100

    Add create_datasets helper

--- 6_langs/utils/helpers.py
+++ 6_langs/utils/helpers.py
@@ -1,6 +1,17 @@
 import re
+import os
 
 def words_from_line(line: str) -> list:
     "Zwraca listę słów dla linijki tekstu unicode."
     words = re.split(r'[\W\d]+', line)
     return [w.lower() for w in words if w]
+
+def create_datasets(languages: dict, handler_class: 'Handler', ngram_class: 'Ngram') -> dict:
+    language_datasets = dict()
+    os.chdir(os.getcwd() + "/texts")
+    for language in languages:
+        handler = handler_class(languages[language])
+        ngram = ngram_class(handler.words)
+        language_datasets[language] = ngram.get_dataset()
+
+    return language_datasets
