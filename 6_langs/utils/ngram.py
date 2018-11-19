b3ceba9efcb90691a78c1718e5ffde96d140a8ea
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-11-19 07:26:21 +0100

    Update Ngram class to return dataset (1-grams, 2-grams, 3-grams etc. composed)

--- 6_langs/utils/ngram.py
+++ 6_langs/utils/ngram.py
@@ -13,8 +13,24 @@ class Ngram:
     def _get_ngrams(self, size: int = 1) -> list:
         return ngrams(self.string, size)
 
+    def _iterate_multiplicity(self, ngrams_generator: iter) -> list():
+        ngrams = list()
+        while True:
+            try:
+                ngrams.append(next(ngrams_generator))
+            except StopIteration:
+                return ngrams
+
     def get_data(self, size: int = 1) -> Counter:
         return Counter(self._get_ngrams(size))
+
+    def get_dataset(self, multiplicity: int = 3) -> Counter:
+        ngrams = list()
+        for factor in range(1, multiplicity + 1):
+            ngrams_generator = self._get_ngrams(factor)
+            ngrams += self._iterate_multiplicity(ngrams_generator)
+        
+        return Counter(ngrams)
 
     def __str__(self) -> str:
         return str(self.words)
