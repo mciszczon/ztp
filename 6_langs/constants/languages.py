3751ba043f132394f03608146a7a2ba7adc1de70
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-11-19 07:25:39 +0100

    Add langs contstants

---
+++ 6_langs/constants/languages.py
@@ -0,0 +1,21 @@
+POL = 'Polish'
+ENG = 'English'
+FIN = 'Finnish'
+GER = 'German'
+ITA = 'Italian'
+SPA = 'Spanish'
+
+
+LANGUAGES = {
+    POL: ['pol1.txt', 'pol2.txt', 'pol3.txt'],
+    ENG: ['eng1.txt', 'eng2.txt', 'eng3.txt', 'eng4.txt'],
+    FIN: ['fin1.txt', 'fin2.txt'],
+    GER: ['ger1.txt', 'ger2.txt', 'ger3.txt', 'ger4.txt'],
+    ITA: ['ita1.txt', 'ita2.txt'],
+    SPA: ['spa1.txt', 'spa2.txt'],
+}
+
+TEST_LANGS = {
+    POL: ['pol1.txt'],
+    ENG: ['eng1.txt'],
+}
