9ef9740996c8ff17f1e01c4d75080de18a9400b1
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-11-19 07:26:31 +0100

    update handler to be able to work on many files at once

--- 6_langs/utils/handler.py
+++ 6_langs/utils/handler.py
@@ -1,25 +1,25 @@
 import re
+from .helpers import words_from_line
 
 class Handler:
     """
     File handler
     """
-    def __init__(self, filename: str) -> None:
-        self.filename = filename
 
-    def __words_from_line(self, line: str) -> list:
-        "Zwraca listę słów dla linijki tekstu unicode."
-        words = re.split(r'[\W\d]+', line)
-        return [w.lower() for w in words if w]
+    def __init__(self, filenames: list) -> None:
+        self.filenames = filenames
 
     @property
-    def lines(self) -> list:
-        with open(self.filename, "r") as file:
-            return file.readlines()
-
+    def files_contents(self) -> list:
+        lines = list()
+        for filename in self.filenames:
+            with open(filename, "r") as file:
+             lines += file.readlines()
+        
+        return lines
     @property
     def words(self) -> list:
         words = []
-        for line in self.lines:
-            [words.append(word) for word in self.__words_from_line(line)]
+        for line in self.files_contents:
+            [words.append(word) for word in words_from_line(line)]
         return words
