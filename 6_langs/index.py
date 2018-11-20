c2405511d4690128046be4fe158231350f5c2bed
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-11-20 14:49:06 +0100

    Update cosinus calc function

--- 6_langs/index.py
+++ 6_langs/index.py
@@ -6,16 +6,6 @@ from utils.calcs import cosinus_similiarity
 from constants.languages import TEST_LANGS, LANGUAGES, POL, ENG, GER, ITA, SPA, FIN
 
 """
-1. √    Tworzę 1-, 2-, 3-gramy (może być więcej) dla obu porównywanych języków (tekstów). W ten sposób powstaje `x` i `y`.
-        Porównuję tylko ngramy o takiej samej krotności! Czyli 1-gramy `x` z 1-gramami `y` itd.
-        Są one częścią tego samego wektora, np. { 'a': 312, ..., 'ab': 12, ... }
-2. √    x1 i y1 to odpowiadające sobie ngramy, np. 'al' i 'al' – a dokładniej ich wartości (częstością występowania).
-3. √    Jeżeli dany ngram nie wystąpi w drugim tekście, to jego wartość powinna być przyjęta jako 0.
-
-Długość wektora len(x): √x1^2 + x2^2 + x3^2 + ... + xn^2 (pierwiastek nad całością)
-"""
-
-"""
 Main Block
 """
 
@@ -25,11 +15,13 @@ def main():
     phrase_ngram = Ngram(words_from_line(phrase))
 
     phrase_dataset = phrase_ngram.get_dataset()
-    languages_datasets = create_datasets(TEST_LANGS, Handler, Ngram)
+    languages_datasets = create_datasets(LANGUAGES, Handler, Ngram)
     languages_similiarity = dict()
 
-    for language in TEST_LANGS:
+    for language in LANGUAGES:
         languages_similiarity[language] = cosinus_similiarity(phrase_dataset, languages_datasets[language])
+
+    print(languages_similiarity)
         
     return True
 
