9e6c9222f63eb672102f25a565db33a07f596846
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-11-21 18:05:31 +0100

    Write euclides similiarity function (WIP)

--- 6_langs/utils/calcs.py
+++ 6_langs/utils/calcs.py
@@ -1,6 +1,11 @@
 from math import sqrt
+from constants.calcs import COS, EUC
+
 
 def cosinus_similiarity(phrase_dataset: dict, language_dataset: dict) -> float:
+    """
+    Returns a float between 0 and 1, where the lower result means more similiarity.
+    """
     summed = 0
     len_phrase = 0
     len_language = 0
@@ -11,3 +16,20 @@ def cosinus_similiarity(phrase_dataset: dict, language_dataset: dict) -> float:
         len_language += language_dataset[ngram]**2
 
     return 1 - ( summed / ( sqrt(len_phrase) * sqrt(len_language) ) )
+
+
+def euclides_similiarity(phrase_dataset: dict, language_dataset: dict) -> float:
+    """
+    TODO: What should it return?
+    """
+    summed = 0
+    for ngram in language_dataset.keys():
+        summed += (language_dataset[ngram] - (phrase_dataset.get(ngram) or 0))**2
+    
+    return sqrt(summed)
+
+
+calc_functions = {
+    COS: cosinus_similiarity,
+    EUC: euclides_similiarity,
+}
