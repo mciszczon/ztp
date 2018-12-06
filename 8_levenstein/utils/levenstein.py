f81aa066ce64a0f427018cdbaa20b9aa3113129b
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-12-06 12:17:56 +0100

    Refactor, add comments

--- 8_levenstein/utils/levenstein.py
+++ 8_levenstein/utils/levenstein.py
@@ -2,26 +2,35 @@ from .matrix import Matrix
 
 
 class Levenstein:
-
+    """
+    Handles the creation of Matrix from given words,
+    and calculates the Levenstein distance between them.
+    """
     def __init__(self, word1: str, word2: str) -> None:
         self.word1 = word1
         self.word2 = word2
         self.matrix = Matrix(word1, word2)
         self.array = self.matrix.array
-        self.__calculate()
+        self.__fill()
 
-    def __calculate(self) -> int:
+    def __calculus(self, row: int, col: int) -> int:
+        """ Calculate value for given (x, y) cell """
+        val1 = self.array[row][col-1] + 1
+        val2 = self.array[row-1][col] + 1
+        val3 = self.array[row-1][col-1] + 1
+
+        if self.word1[col-1] == self.word2[row-1]:
+            val3 -= 1
+
+        return min(val1, val2, val3)
+
+    def __fill(self) -> int:
+        """ Fill the matrix with computed values """
         for row_i, row in enumerate(self.array):
             for col_i, col in enumerate(row):
                 if col_i > 0 and row_i > 0:
-                    var1 = self.array[row_i][col_i-1] + 1
-                    var2 = self.array[row_i-1][col_i] + 1
-                    var3 = self.array[row_i-1][col_i-1] + 1
-
-                    if self.word1[col_i-1] == self.word2[row_i-1]:
-                        var3 -= 1
-
-                    self.array[row_i][col_i] = min(var1, var2, var3)
+                    self.array[row_i][col_i] = self.__calculus(row_i, col_i)
 
     def __str__(self) -> str:
+        """ Allow getting the result by simply calling the instance """
         return "{}/{}: {}".format(self.word1, self.word2, self.matrix.right_bottom)
--- 8_levenstein/utils/matrix.py
+++ 8_levenstein/utils/matrix.py
@@ -1,4 +1,8 @@
 class Matrix:
+    """
+    Handles the creation, modification and read-operations
+    on a 2-dimensional array (a Matrix)
+    """
     width = 0
     height = 0
     dimensions = (0, 0)
@@ -8,16 +12,16 @@ class Matrix:
         self.width = len(word1) + 1
         self.height = len(word2) + 1
         self.dimensions = (self.width, self.height)
-        self.__initialize()
-
-    def __initialize(self) -> None:
-        self.array = self.__generate_2d_array()
+        
+        self.__generate_2d_array()
         self.__enum_words()
 
-    def __generate_2d_array(self) -> list:
-        return [[0 for x in range(self.width)] for y in range(self.height)]
+    def __generate_2d_array(self) -> None:
+        """ Creates the array and fills it with 0s"""
+        self.array = [[0 for x in range(self.width)] for y in range(self.height)]
 
     def __enum_words(self) -> None:
+        """ Fills first row and first column with incremental values"""
         for i in range(self.width):
             self.array[0][i] = i
         for i in range(self.height):
@@ -25,4 +29,5 @@ class Matrix:
 
     @property
     def right_bottom(self) -> int:
+        """ Returns the right-bottom value from matrix"""
         return self.array[-1][-1]
